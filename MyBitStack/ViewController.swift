bf278bc62277b909e2e78d5b1cec052ffcffb153
Ziad Hamdieh <ziad@Ziads-MacBook-Pro.local>
2018-05-27 03:08:09 -0400

    organize project structure into MVC

--- MyBitStack/ViewController.swift
+++ MyBitStack/ViewController.swift
@@ -7,15 +7,37 @@
 //
 
 import UIKit
+import CoreLocation
+import Alamofire
+import SwiftyJSON
+import SVProgressHUD
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    let currencies : [String] = []
+    let BITCOIN_URL : String = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC"
+    
     @IBOutlet weak var priceLabel: UILabel!
     @IBOutlet weak var currencyPicker: UIPickerView!
+    
+    let locationManager = CLLocationManager()
+    
+    // Networking
+    func getBitcoinData() {
+        
+    }
+    
+    func updateBitcoinData() {
+    }
+    
+    func updateUI() {
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
