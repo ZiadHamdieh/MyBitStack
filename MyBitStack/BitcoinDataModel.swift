9715e975b297abf786a89424714794f9c549a300
Ziad Hamdieh <ziad@Ziads-MacBook-Pro.local>
2018-05-27 04:25:14 -0400

    add data model file

---
+++ MyBitStack/BitcoinDataModel.swift
@@ -0,0 +1,16 @@
+//
+//  BitcoinDataModel.swift
+//  MyBitStack
+//
+//  Created by Ziad Hamdieh on 2018-05-27.
+//  Copyright © 2018 Ziad Hamdieh. All rights reserved.
+//
+
+import UIKit
+
+class BitcoinDataModel {
+    
+    var priceThisHour : Double = 0.00
+    
+}
+
