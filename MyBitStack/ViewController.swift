335f5507856567972cbe4c65288303dcea6f4aba
Ziad Hamdieh <ziad@Ziads-MacBook-Pro.local>
2018-06-02 17:34:51 -0400

    refactor code for readability

--- MyBitStack/ViewController.swift
+++ MyBitStack/ViewController.swift
@@ -27,30 +27,39 @@ class ViewController: UIViewController {
     
     //MARK: - View Lifecycle
     /*****************************************************************/
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // set this class as the delegate for UIPicker
         currencyPicker.delegate = self
         currencyPicker.dataSource = self
+        
     }
     
     //MARK: - Networking
     /*****************************************************************/
+    
     func getBitcoinPrice(url: String) {
         if API_URL == BITCOIN_URL_ROOT {
+            
             priceLabel.text = ""
+            
         }
         else {
             Alamofire.request(url, method: .get).responseJSON {
                 response in
                 if response.result.isSuccess {
+                    
                     let bitcoinJSON : JSON = JSON(response.result.value!)
                     self.updateBitcoinData(data: bitcoinJSON)
+                    
                 }
                 else {
+                    
                     print("coult not get bitcoin data")
                     self.priceLabel.text = "Fetch Error"
+                    
                 }
             }
         }
@@ -58,35 +67,45 @@ class ViewController: UIViewController {
 
     //MARK: - PARSING
     /*****************************************************************/
-    // parse the JSON for desired information
+    
     func updateBitcoinData(data: JSON) {
         // optional binding used here to avoid forced unwrapping
         if let hourPriceResult = data["open"]["hour"].double {
+            
             bitcoinDataModel.priceThisHour = hourPriceResult
             bitcoinDataModel.percentChange = data["changes"]["percent"]["hour"].doubleValue
             bitcoinDataModel.currencySymbol = chosenCurrency
             updateUI()
+            
         }
         else {
+            
             print("Bitcoin prices currently unavailable")
             priceLabel.text = "Unavailable"
+            
         }
     }
     
     func updateUI() {
         // if bitcoin prices have fallen since last hour, display in red
         if bitcoinDataModel.percentChange < 0 {
+            
             priceLabel.textColor = .red
             percentageChangeLabel.textColor = .red
             percentageChangeLabel.text = "-\(bitcoinDataModel.percentChange)%"
+            
         }
         // else display value in green
         else if bitcoinDataModel.percentChange > 0 {
+            
             priceLabel.textColor = .green
             percentageChangeLabel.textColor = .green
             percentageChangeLabel.text = "+\(bitcoinDataModel.percentChange)%"
+            
         }
+        
         priceLabel.text = "\(bitcoinDataModel.currencySymbol)\(bitcoinDataModel.priceThisHour)"
+        
     }
     
 }
@@ -95,22 +114,29 @@ extension ViewController: UIPickerViewDataSource, UIPickerViewDelegate {
     
     // returns the number of columns in the UIPicker
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        
         return 1
+        
     }
     
     // number of rows in the UIPicker is equal to the number of elements in the
     // currency array
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        
         return currencies.count
+        
     }
     
     // place each element in the currency array into its respective row within the UIPicker
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
         return currencies[row]
+        
     }
     
     // print the currency in the row currently selected within the UIPicker
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
         API_URL = BITCOIN_URL_ROOT + currencies[row]
         chosenCurrency = currencySymbols[row]
         getBitcoinPrice(url: API_URL)
@@ -119,21 +145,27 @@ extension ViewController: UIPickerViewDataSource, UIPickerViewDelegate {
     
     // change color of each row to white
     func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        
         var label : UILabel
         
         if let view = view as? UILabel {
+            
             label = view
+            
         }
         else {
+            
             label = UILabel()
+            
         }
+        
         label.textColor = .white
         label.textAlignment = .center
         label.font = UIFont(name: "Menlo-Regular", size: 22)
-        
         label.text = currencies[row]
         
         return label
+        
     }
     
 }
